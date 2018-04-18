afcdb1b67bdbbecc4bb5acdebc01b3554c7cc0d8
hanyun <1355081829@qq.com>
2018-04-18 08:56:24 +0800

    修改单元测试错误

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,47 +1,7 @@
 <template>
     <div>
-        <div class="header">
-            <!--<mu-appbar title="">-->
-            <!--<mu-icon-button icon="search" slot="right"/>-->
-            <!--</mu-appbar>-->
-
-            <mu-tabs :value="activeTab" @change="handleTabChange">
-                <mu-tab value="school" title="校园"/>
-                <mu-tab value="shetuan" title="社团"/>
-                <mu-tab value="class" @active="handleActive" title="班级"/>
-            </mu-tabs>
-
-        </div>
-        <div>
-            <div v-if="activeTab === 'school'">
-                <h2>校园</h2>
-                <p>
-                    这是第一个 tab
-                </p>
-            </div>
-            <div v-if="activeTab === 'shetuan'">
-                <h2>社团</h2>
-                <p>
-                    这是第二个 tab
-                </p>
-            </div>
-            <div v-if="activeTab === 'class'">
-                <h2>班级</h2>
-                <p>
-                    这是第三个 tab
-                </p>
-            </div>
-        </div>
-        <div class="footer">
-            <mu-paper>
-                <mu-bottom-nav :value="bottomNav" shift @change="handleChange">
-                    <mu-bottom-nav-item value="home" title="关注" icon="home"/>
-                    <mu-bottom-nav-item value="explore" title="发现" icon="explore"/>
-                    <mu-bottom-nav-item value="group" title="校园" icon="school"/>
-                    <mu-bottom-nav-item value="alert" title="消息" icon="add_alert"/>
-                    <mu-bottom-nav-item value="account_circle" title="我的" icon="account_circle"/>
-                </mu-bottom-nav>
-            </mu-paper>
+        <div class="hello">
+            <h1>Welcome to Your Vue.js App</h1>
         </div>
     </div>
 </template>
--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,5 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-// import HelloWorld from '@/components/HelloWorld'
 import Home from '@/page/home/Home'
 import School from '@/page/school/School'
 import Message from '@/page/message/Message'
