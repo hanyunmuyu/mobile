3db10aaca70e6403b476bb9c413613dffa74f0ec
hanyun <1355081829@qq.com>
2018-04-27 16:26:36 +0800

    点赞，收藏

--- src/page/club/activity/Detail.vue
+++ src/page/club/activity/Detail.vue
@@ -113,7 +113,8 @@ export default {
       index: 1,
       value: true,
       toast: false,
-      msg: ''
+      msg: '',
+      tag: 'club'
     }
   },
   mounted () {
@@ -132,7 +133,7 @@ export default {
         this.toast = false
       }, 2000)
       let id = this.$route.params.id
-      this.$service.recommendFavorite(this.$api.recommendFavoriteUrl, {id: id}).then((r) => {
+      this.$service.recommendFavorite(this.$api.recommendFavoriteUrl, {id: id, tag: this.tag}).then((r) => {
         if (r.code === 2000) {
           this.icon = this.icon === 'favorite_border' ? 'favorite' : 'favorite_border'
         }
@@ -149,7 +150,7 @@ export default {
     },
     init () {
       let id = this.$route.params.id
-      this.$service.recommendFavoriteDetail(this.$api.recommendFavoriteDetailUrl, {id: id}).then((r) => {
+      this.$service.recommendFavoriteDetail(this.$api.recommendFavoriteDetailUrl, {id: id, tag: this.tag}).then((r) => {
         if (r.code === 2000) {
           if (r.data.favorite === 1) {
             this.icon = 'favorite'
