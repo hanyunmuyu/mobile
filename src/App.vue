d483de91b0a8d38b27d9c8c2a665e9c08a558b5b
hanyun <1355081829@qq.com>
2018-05-16 13:57:31 +0800

    优化底部导航显示

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,12 @@
 <template>
-    <div id="app">
+    <div id="app" ref="app">
         <!--<img src="/static/logo.png">-->
         <keep-alive>
             <router-view v-if="$route.meta.keepAlive"></router-view>
         </keep-alive>
         <router-view v-if="!$route.meta.keepAlive"></router-view>
         <div style="height: 50px"></div>
-        <BottomNav></BottomNav>
+        <BottomNav v-show="show"></BottomNav>
     </div>
 </template>
 
@@ -15,6 +15,30 @@ import BottomNav from '@/pages/common/BottomNav'
 
 export default {
   name: 'App',
+  data () {
+    return {
+      show: true,
+      scrollTop: 0
+    }
+  },
+  mounted () {
+    let scrollTop = window.scrollY
+    this.scrollTop = scrollTop
+    window.addEventListener('touchstart', this.showBottom)
+    window.addEventListener('touchmove', this.winScroll)
+  },
+  methods: {
+    winScroll () {
+      let scrollTop = window.scrollY
+      if (Math.abs(scrollTop - this.scrollTop) > 30) {
+        this.show = false
+        this.scrollTop = scrollTop
+      }
+    },
+    showBottom () {
+      this.show = true
+    }
+  },
   components: {BottomNav}
 }
 </script>
